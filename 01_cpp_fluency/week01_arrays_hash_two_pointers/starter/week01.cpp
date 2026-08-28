f1b4ac0fec805f1a0929fd6a77cae8866b31d0f9
Demitri Gentile <demitgent0222@gmail.com>
2026-08-28 11:09:29 -0700

    01W1 Complete

--- 01_cpp_fluency/week01_arrays_hash_two_pointers/starter/week01.cpp
+++ 01_cpp_fluency/week01_arrays_hash_two_pointers/starter/week01.cpp
@@ -1,31 +1,45 @@
 #include "week01.hpp"
 
 #include <stdexcept>
-#include<unordered_map>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
 
 namespace cpp_course::week01 {
 
-bool has_duplicate(const std::vector<int>& values) {
-  if (values.size()<= 1) {return false;}
+bool has_duplicate(const std::vector<int>& values) 
+{
+  if (values.size()<= 1) 
+  { return false; }
+
+  std::unordered_set<int> value_index;
   for (int i = 0; i < values.size(); i++)
   {
-    for (int j = i+1; j<values.size(); j++ )
-    {
-      if (values[i]==values[j]) {return true;}
-    }
-  }  
+    if(value_index.find(values[i])==value_index.end())
+    { value_index.insert(values[i]); }
+
+    else 
+    { return true; }
+  }
+
   return false;
 }
 
-std::optional<IndexPair> two_sum_indices(const std::vector<int>& values, int target) {
-  if (values.size()<=1){return std::nullopt;}
+std::optional<IndexPair> two_sum_indices(const std::vector<int>& values, int target) 
+{
+  if (values.size()<=1)
+  { return std::nullopt; }
+  
+  std::unordered_map<int,int> value_index; //value, index
   for (int i = 0; i < values.size(); i++)
   {
-    for (int j = i+1; j<values.size(); j++)
-    {
-      if (values[i]+values[j]==target) { return IndexPair{i,j}; }
-    }
+    if (value_index.find(target-values[i])!= value_index.end())
+    { return IndexPair{value_index[target-values[i]],i}; }
+
+    else
+    { value_index[values[i]]=i; }
   }
+
   return std::nullopt;
 }
 
@@ -39,37 +53,61 @@ bool are_anagrams(std::string_view left, std::string_view right)
     count[static_cast<unsigned char>(left[i])] += 1;
     count[static_cast<unsigned char>(right[i])] -= 1;
   }
+
   for (int i = 0; i < 256; i++)
   {
-    if (count[i]!=0) {return false;}
+    if (count[i]!=0) 
+    { return false; }
   }
+
   return true;
 }
 
 std::size_t deduplicate_sorted(std::vector<int>& sorted_values) {
-  if (sorted_values.size()<=1){return sorted_values.size();}
-  int c = 1;
-  std::optional<int> x = std::nullopt;
+  if (sorted_values.size()<=1)
+  { return sorted_values.size(); }
+
+  int size_counter = 1;
+  std::optional<int> duped_index = std::nullopt;
   for (int i = 1; i < sorted_values.size(); i++)
   {
-    if (sorted_values[i] == sorted_values[i-1] && !x.has_value())
+    if (sorted_values[i] == sorted_values[i-1] && !duped_index.has_value())
+    { duped_index = i; }
+
+    else if (duped_index && sorted_values[i] != sorted_values[i-1])
     {
-      x = i;
+      sorted_values[duped_index.value()] = sorted_values[i];
+      duped_index = duped_index.value() + 1;
+      size_counter++;
     }
-    else if (x && sorted_values[i] != sorted_values[i-1])
-    {
-      sorted_values[x.value()] = sorted_values[i];
-      x = x.value() + 1;
-      c++;
-    }
-    else if (!x.has_value() && sorted_values[i] != sorted_values[i-1]){c++;}
+
+    else if (!duped_index.has_value() && sorted_values[i] != sorted_values[i-1])
+    { size_counter++; }
   }
-  return c;
+
+  return size_counter;
 }
 
-long long max_container_area(const std::vector<int>& heights) {
-  (void)heights;
-  throw std::logic_error("TODO: implement max_container_area");
+long long get_area(const int &side1,const int &side2, const int &distance)
+{
+  return static_cast<long long>(std::min(side1,side2)) * static_cast<long long>(distance);
+}
+
+long long max_container_area(const std::vector<int>& heights) 
+{
+  if (heights.size() < 2){return 0;}
+  int left = 0, right = heights.size()-1;
+  long long area = 0;
+  while (left<right)
+  {
+    long long new_area = get_area(heights[left],heights[right], right-left);
+    if (new_area > area) 
+    { area = new_area; }
+
+    if(heights[left]>heights[right]) { right--; }
+    else { left++; }
+  }
+  return area;
 }
 
 }  // namespace cpp_course::week01
